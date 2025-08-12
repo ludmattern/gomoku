a8b6e3a73b57e4a01490f88cc2bbc851dd8a88d6
Jordan Gavairon <jass@jordans-macbook-air.home>
2025-08-13 00:50:27 +0200

    feat: added visualizer for game board. and handle click event to place a pawn black or white

--- src/gui/GameBoardRenderer.cpp
+++ src/gui/GameBoardRenderer.cpp
@@ -1,4 +1,5 @@
 #include "GameBoardRenderer.hpp"
+#include <iostream>
 
 GameBoardRenderer::GameBoardRenderer(void)
 {
@@ -33,23 +34,74 @@ void GameBoardRenderer::updateCell(int x, int y, CellState state)
 void GameBoardRenderer::render(sf::RenderWindow& window)
 {
     const int gridSize = 600;
-    const int cellSize = gridSize / 18;
+    const int cellSize = gridSize / 19;
     const int startX = (1920 - gridSize) / 3;
     const int startY = (1080 - gridSize) / 4;
 
+    // Changer de 19 à 20 lignes
+    // for (int i = 0; i < 20; i++)  // 20 lignes horizontales
+    // {
+    //     sf::RectangleShape line(sf::Vector2f(gridSize - 10, 2));
+    //     line.setPosition(sf::Vector2f(startX, startY + i * cellSize));
+    //     line.setFillColor(sf::Color::White);
+    //     window.draw(line);
+    // }
+
+    // for (int j = 0; j < 20; j++)  // 20 lignes verticales
+    // {
+    //     sf::RectangleShape line(sf::Vector2f(2, gridSize - 10));
+    //     line.setPosition(sf::Vector2f(startX + j * cellSize, startY));
+    //     line.setFillColor(sf::Color::White);
+    //     window.draw(line);
+    // }
+    
+    // Dessiner la grille noire (intersections au centre des cases blanches)
+    const int blackGridStartX = startX + cellSize / 2;  // Décalé de la moitié d'une case
+    const int blackGridStartY = startY + cellSize / 2;  // Décalé de la moitié d'une case
+
     for (int i = 0; i < 19; i++)
     {
-        sf::RectangleShape line(sf::Vector2f(gridSize, 2));
-        line.setPosition(sf::Vector2f(startX, startY + i * cellSize));  // Correction ici
+        sf::RectangleShape line(sf::Vector2f(gridSize - (cellSize + 10), 2));  // Plus fine
+        line.setPosition(sf::Vector2f(blackGridStartX, blackGridStartY + i * cellSize));
         line.setFillColor(sf::Color::White);
         window.draw(line);
     }
 
     for (int j = 0; j < 19; j++)
     {
-        sf::RectangleShape line(sf::Vector2f(2, gridSize));
-        line.setPosition(sf::Vector2f(startX + j * cellSize, startY));  // Correction ici
+        sf::RectangleShape line(sf::Vector2f(2, gridSize - (cellSize + 10)));  // Plus fine
+        line.setPosition(sf::Vector2f(blackGridStartX + j * cellSize, blackGridStartY));
         line.setFillColor(sf::Color::White);
         window.draw(line);
     }
+
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+            switch (_board[i][j])
+            {
+                case CellState::Player1:
+                {
+                    sf::CircleShape circle(cellSize / 3);
+                    circle.setPosition(sf::Vector2f(startX + i * cellSize + cellSize / 6, startY + j * cellSize + cellSize / 6));
+                    circle.setFillColor(sf::Color::White);
+                    window.draw(circle);
+                    break;
+                }
+                case CellState::Player2:
+                {
+
+                    sf::CircleShape circle(cellSize / 3);
+                    circle.setPosition(sf::Vector2f(startX + i * cellSize + cellSize / 6, startY + j * cellSize + cellSize / 6 ));
+                    circle.setFillColor(sf::Color::Black);
+                    window.draw(circle);
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    }
+
 }
--- src/gui/GameWindow.cpp
+++ src/gui/GameWindow.cpp
@@ -26,6 +26,11 @@ void GameWindow::init(void)
 
 void GameWindow::handleEvents(void)
 {
+    const int gridSize = 600;
+    const int startX = (1920 - gridSize) / 3;
+    const int startY = (1080 - gridSize) / 4;
+    const int cellSize = gridSize / 19;
+    
     while (auto event = _window.pollEvent())
     {
         if (event->is<sf::Event::Closed>())
@@ -35,6 +40,39 @@ void GameWindow::handleEvents(void)
             auto key = event->getIf<sf::Event::KeyPressed>()->code;
             if (key == sf::Keyboard::Key::Escape)
                 cleanup();
+        }
+        if (event->is<sf::Event::MouseButtonPressed>())
+        {
+            auto mouseClick = event->getIf<sf::Event::MouseButtonPressed>()->button;
+            if (mouseClick == sf::Mouse::Button::Left)
+            {
+                auto mousePosition = _window.mapPixelToCoords(sf::Mouse::getPosition(_window));
+                if (mousePosition.x > startX && mousePosition.x < startX + gridSize && mousePosition.y > startY && mousePosition.y < startY + gridSize)
+                {
+                    int gridX = (mousePosition.x - startX) / cellSize;
+                    int gridY = (mousePosition.y - startY) / cellSize;
+                    std::cout << "Mouse clicked at: " << mousePosition.x << ", " << mousePosition.y << std::endl;
+                    if (gridX >= 0 && gridX < 19 && gridY >= 0 && gridY < 19)
+                    {
+                        std::cout << "Case clicked at: " << static_cast<int>(gridX) << ", " << static_cast<int>(gridY) << std::endl;
+                        _boardRenderer.updateCell(gridX, gridY, CellState::Player1);  // Correction ici
+                    }
+                }
+            }
+            if (mouseClick == sf::Mouse::Button::Right)
+            {
+                auto mousePosition = _window.mapPixelToCoords(sf::Mouse::getPosition(_window));
+                if (mousePosition.x > startX && mousePosition.x < startX + gridSize && mousePosition.y > startY && mousePosition.y < startY + gridSize)
+                {
+                    int gridX = (mousePosition.x - startX) / cellSize;
+                    int gridY = (mousePosition.y - startY) / cellSize;
+                    if (gridX >= 0 && gridX < 19 && gridY >= 0 && gridY < 19)
+                    {
+                        std::cout << "Case clicked at: " << static_cast<int>(gridX) << ", " << static_cast<int>(gridY) << std::endl;
+                        _boardRenderer.updateCell(gridX, gridY, CellState::Player2);  // Correction ici
+                    }
+                }
+            }
         }
     }
 }
